244aa2f07363a927eee8accc8f1c520a5d74ed28
nickarora <arora.nick@gmail.com>
2015-02-20 04:47:12 -0800

    set up basic structure

---
+++ js-theme/hide-nav.js
@@ -0,0 +1,8 @@
+  var offset = 740; // some offset value for which when the header becomes hidden
+  jQuery(window).scroll(function() { //also an option: jQuery .on('scroll') method
+    if(jQuery('#hide-bar').is(':visible') && jQuery(window).scrollTop() < offset) {
+      jQuery('#hide-bar').fadeOut(200);
+    } else if(!jQuery('#hide-bar').is(':visible') && jQuery(window).scrollTop() > offset) {
+      jQuery('#hide-bar').fadeIn(400);
+    }    
+  });
