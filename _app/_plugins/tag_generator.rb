d2dbd4ed83591e6b1227f12adf73ca91df8e9585
Nick Arora <arora.nick@gmail.com>
2024-06-01 22:01:27 -0700

    Tag pages

---
+++ _app/_plugins/tag_generator.rb
@@ -0,0 +1,34 @@
+def generate_tags(post)
+  tags_directory = File.expand_path("../_tags", __dir__)
+  tag_file_extension = ".md"
+
+  filenames = Dir.entries(tags_directory)
+  filenames = filenames.filter do |filename|
+    extension = File.extname(filename)
+    extension == tag_file_extension
+  end
+
+  generated_tags = filenames.map do |filename|
+    File.basename(filename, tag_file_extension)
+  end
+
+  tags = post["tags"]
+  tags = tags.reject(&:empty?)
+
+  tags.each do |tag|
+    if generated_tags.include?(tag)
+      next
+    end
+
+    File.open("#{tags_directory}/#{tag}#{tag_file_extension}", "wb") do |file|
+      file.puts("---")
+      file.puts("layout: tag")
+      file.puts("tag-name: #{tag}")
+      file.puts("---")
+    end
+  end
+end
+
+Jekyll::Hooks.register :posts, :post_write do |post|
+  generate_tags(post)
+end
